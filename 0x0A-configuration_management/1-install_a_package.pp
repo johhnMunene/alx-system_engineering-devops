03b973a5ec9523bc06863d44568989b38dd9a7db
johhnmunene <edwardsjohnny64@.com>
2023-11-24 19:45:03 -0600

    Puppet, install flask from pip3 mod

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-#Using Puppet, install flask from pip3.
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+#install package
+package{'flask':
+        ensure   => '2.1.0',
+        provider => 'pip3'
 }
-
