e0c6c005936aa0a4d93cd4c9da4b91651ba8d504
johhnmunene <edwardsjohnny64@.com>
2023-11-27 07:26:05 -0600

    Dissable password

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,9 @@
 # Disable password authentication
-
 file { '/etc/ssh/ssh_config':
   ensure  => present,
-  content =>"Host 246582-web-01/n/
-             HostName 54.237.68.41/n/
-	     identityFile ~/.ssh/school/n/
-             PasswordAuthentication No"
-	     
+  content => "Host 239778-web-01\n\
+                  HostName 52.87.235.104\n\
+                  User mally13\n\
+                  IdentityFile ~/.ssh/school\n\
+                  PasswordAuthentication no\n",
 }
