55f35cd310fce048d3ec89f9be1bfafe97925c64
johhnmunene <edwardsjohnny64@.com>
2023-11-29 18:44:13 -0600

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,5 @@
-# install package
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'flask_install':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  refreshonly => true,
-}
-
-package { 'flask':
-  ensure   => installed,
-  provider => pip3,
-  require  => Exec['flask_install'],
+#install package
+package{'flask':
+        ensure   => '2.1.0',
+        provider => 'pip3'
 }
