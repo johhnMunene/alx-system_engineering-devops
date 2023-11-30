f6b9a6dc30afc6391b6286c935987c322ccc621c
johhnmunene <edwardsjohnny64@.com>
2023-11-29 18:57:33 -0600

    1-install_a_package.pp modd

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,10 @@
-#install package
-package{'flask':
-        ensure   => '2.1.0',
-        provider => 'pip3'
+# install package
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-package{'Werkzeug':
-	ensure  => '2.1.1',
-	provider  => 'pip3',
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
